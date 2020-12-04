84839ab0e084bda5fe29a97987dedf4583a8afcd
najimRizky <najim.rizky@student.umn.ac.id>
2020-12-04 19:11:39 +0700

    Repair search bar (finished)

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@
           </v-list>
         </v-menu>
       </v-tabs>
-      <v-text-field class="expanding-search" placeholder="Search" filled dense color="grey" prepend-inner-icon="mdi-magnify" 
+      <v-text-field class="expanding-search" placeholder="Search" v-if="searchIndicator" filled dense color="grey" prepend-inner-icon="mdi-magnify" 
       v-on:keyup.enter="gotoSearch()" v-model="searchKeyword" style="margin-bottom: -25px;"></v-text-field>
 
     </div>

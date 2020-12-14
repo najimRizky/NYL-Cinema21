1e33d36f24ebb8a293f12715a67a7cac177fce43
yuannfelixx <yuan.felix@student.umn.ac.id>
2020-12-14 21:26:29 +0700

    aboutus tanpa najim

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,103 @@
 <template>
+  <div>
+    <v-layout row wrap justify-center>
+    <div class="about">
+    <v-card
+    :loading="loading"
+    class="mx-4 my-12"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="@/assets/yuan.jpg"
+    ></v-img>
+
+    <v-card-text>
+    <div> Name : Yuan Felix Iskandar </div>
+    <div> Date of Birth : 23 April 2001 </div>
+    <div> Age  : 19  </div>
+    <div> NIM  : 00000037867 </div><br>
+    <div> <v-col cols="12" align="center">
+        Motto: <br>
+      'lorem ipsum dolor si amet,consectetur adipiscing elit. Maecenas ornare convallis consectetur. 
+       Nulla id quam fermentum, suscipit est in, posuere enim'</v-col></div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-col cols="12" align="center">
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-facebook
+      </v-icon>
+    </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-twitter
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-instagram
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+      <v-icon dark>
+        mdi-linkedin
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+    
+      <v-icon dark>
+        mdi-github
+      </v-icon>
+    </v-btn>
+      </v-col>
+    </v-card-actions>
+  </v-card>
+  </div>
+
   <div class="about">
     <v-card
     :loading="loading"
@@ -15,68 +114,89 @@
 
     <v-img
       height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      src
     ></v-img>
 
-    <v-card-title>Cafe Badilico</v-card-title>
-
     <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ml-4">
-          4.5 (413)
-        </div>
-      </v-row>
-
-      <div class="my-4 subtitle-1">
-        $ • Italian, Cafe
-      </div>
-
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
+    <div> Name : Najim Rizky </div>
+    <div> Date of Birth : </div>
+    <div> Age  : 19  </div>
+    <div> NIM  : 00000040113 </div><br>
+    <div> <v-col cols="12" align="center">
+        Motto: <br>
+      'lorem ipsum dolor si amet,consectetur adipiscing elit. Maecenas ornare convallis consectetur. 
+       Nulla id quam fermentum, suscipit est in, posuere enim'</v-col></div>
     </v-card-text>
 
     <v-card-actions>
+      <v-col cols="12" align="center">
       <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
-        Reserve
-      </v-btn>
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-facebook
+      </v-icon>
+    </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-twitter
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-instagram
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+      <v-icon dark>
+        mdi-linkedin
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+    
+      <v-icon dark>
+        mdi-github
+      </v-icon>
+    </v-btn>
+      </v-col>
     </v-card-actions>
   </v-card>
-  <v-card
+  </div>
+
+  <div class="about">
+    <v-card
     :loading="loading"
     class="mx-4 my-12"
     max-width="374"
@@ -91,148 +211,87 @@
 
     <v-img
       height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      src="@/assets/laras.jpg"
     ></v-img>
 
-    <v-card-title>Cafe Badilico</v-card-title>
-
     <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ml-4">
-          4.5 (413)
-        </div>
-      </v-row>
-
-      <div class="my-4 subtitle-1">
-        $ • Italian, Cafe
-      </div>
-
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
+    <div> Name : Laras Wulandari </div>
+    <div> Date of Birth : 10 Desember 2000 </div>
+    <div> Age  : 20  </div>
+    <div> NIM  : 00000040107 </div><br>
+    <div> <v-col cols="12" align="center">
+        Motto: <br>
+      'Hidup ini penuh kesempatan, 
+      ambil kesempatan itu saat kau di berikan dan jangan menyia - nyiakan karena kita tidak tahu apakah kita akan mendapatkannya lagi'</v-col></div>
     </v-card-text>
 
     <v-card-actions>
+      <v-col cols="12" align="center">
       <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
-        Reserve
-      </v-btn>
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-facebook
+      </v-icon>
+    </v-btn>
+
+      <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-twitter
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+        class="mx-2"
+        fab
+        dark
+        small
+        color="black"
+    >
+      <v-icon dark>
+        mdi-instagram
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+      <v-icon dark>
+        mdi-linkedin
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="black"
+    >
+    
+      <v-icon dark>
+        mdi-github
+      </v-icon>
+    </v-btn>
+      </v-col>
     </v-card-actions>
   </v-card>
-  <v-card
-    :loading="loading"
-    class="mx-4 my-12"
-    max-width="374"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
-
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
-
-    <v-card-title>Cafe Badilico</v-card-title>
-
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ml-4">
-          4.5 (413)
-        </div>
-      </v-row>
-
-      <div class="my-4 subtitle-1">
-        $ • Italian, Cafe
-      </div>
-
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
-        Reserve
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-    <h1>Test coba coba</h1>
-    <h1>Najim Push</h1>
-    <h1>234</h1>
-    <h1>Tes 123</h1>
-    <h1>Hadeh</h1>
-    <h1>Meh</h1>
+  </div>
+    </v-layout>
   </div>
 </template>
+
